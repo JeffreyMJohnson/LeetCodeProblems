a311e22dbfed1e5e6b2e4808817d4cb7dcc2f68a
Jeffrey Johnson <jeffrey.johnson@skykick.com>
2021-03-06 14:57:31 -0800

    Initial commit

---
+++ IPValidation.Tests/Tests.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+
+namespace IPValidation.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestCases()
+        {
+            Assert.AreEqual(true, Kata.is_valid_IP("0.0.0.0"));
+            Assert.AreEqual(true, Kata.is_valid_IP("12.255.56.1"));
+            Assert.AreEqual(true, Kata.is_valid_IP("137.255.156.100"));
+
+            Assert.AreEqual(false, Kata.is_valid_IP(""));
+            Assert.AreEqual(false, Kata.is_valid_IP("abc.def.ghi.jkl"));
+            Assert.AreEqual(false, Kata.is_valid_IP("123.456.789.0"));
+            Assert.AreEqual(false, Kata.is_valid_IP("12.34.56"));
+            Assert.AreEqual(false, Kata.is_valid_IP("12.34.56.00"));
+            Assert.AreEqual(false, Kata.is_valid_IP("12.34.56.7.8"));
+            Assert.AreEqual(false, Kata.is_valid_IP("12.34.256.78"));
+            Assert.AreEqual(false, Kata.is_valid_IP("1234.34.56"));
+            Assert.AreEqual(false, Kata.is_valid_IP("pr12.34.56.78"));
+            Assert.AreEqual(false, Kata.is_valid_IP("12.34.56.78sf"));
+            Assert.AreEqual(false, Kata.is_valid_IP("12.34.56 .1"));
+            Assert.AreEqual(false, Kata.is_valid_IP("12.34.56.-1"));
+            Assert.AreEqual(false, Kata.is_valid_IP("123.045.067.089"));
+        }
+    }
+}
---
+++ IPValidation/Kata.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace IPValidation
+{
+    public class Kata
+    {
+        /*
+         * Write an algorithm that will identify valid IPv4 addresses in dot-decimal format.
+         * IPs should be considered valid if they consist of four octets, with values between 0 and 255,
+         * inclusive.
+
+            Input to the function is guaranteed to be a single string.
+         */
+#pragma warning disable IDE1006 // Naming Styles
+        public static bool is_valid_IP(string ipAddress)
+#pragma warning restore IDE1006 // Naming Styles
+        {
+            if (string.IsNullOrEmpty(ipAddress)) return false;
+
+            var octets = ipAddress.Split('.');
+            if(octets.Length != 4) return false;
+
+            foreach (var octet in octets)
+            {
+                //does octet have whitespace
+                if (Regex.IsMatch(octet, @"\s+")) return false;
+
+                
+
+                //is the octet int from 0 -255
+                if (int.TryParse(octet, out int result) == false) return false;
+                if (result < 0 || result > 255) return false;
+
+                //if result 0 check for more than 1 '0'
+                if (result == 0 && !Regex.IsMatch(octet, @"^[0]{1}$")) return false;
+
+                //does octet have leading 0's
+                if (result != 0 && Regex.IsMatch(octet, @"^[0]{1}")) return false;
+
+            }
+            return true;
+        }
+    }
+}
