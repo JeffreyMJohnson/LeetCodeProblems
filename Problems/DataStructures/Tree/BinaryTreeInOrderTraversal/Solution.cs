8d77331e557ce67f264b2f1d42290fc002c6b707
Jeffrey Johnson <jefjohms@gmail.com>
2021-03-21 15:02:19 -0700

    complete in order tree traversal

---
+++ Problems/DataStructures/Tree/BinaryTreeInOrderTraversal/Solution.cs
@@ -0,0 +1,89 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+using System.Collections.Generic;
+
+namespace Problems.DataStructures.Tree.BinaryTreeInOrderTraversal
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        private static List<int> _resultList;
+
+        public IList<int> InOrderTraversal(TreeNode root, bool useRecursive = true)
+        {
+            _resultList = new List<int>();
+            if (useRecursive)
+            {
+                InOrderRecursive(root);
+            }
+            else
+            {
+              InOrderIterative(root);
+            }
+            return _resultList;
+
+        }
+
+        private void InOrderRecursive(TreeNode root)
+        {
+            if (root == null) return;
+
+            InOrderRecursive(root.left);
+            _resultList.Add(root.val);
+            InOrderRecursive(root.right);
+        }
+
+        private void InOrderIterative(TreeNode root)
+        {
+            if (root == null) return;
+
+            var stack = new Stack<TreeNode>();
+            var leftHasBeenTraversed = new List<TreeNode>();
+
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                
+                var current = stack.Pop();
+
+                if (!leftHasBeenTraversed.Contains(current) && current.left != null)
+                {
+                    leftHasBeenTraversed.Add(current);
+                    stack.Push(current);
+                    stack.Push(current.left);
+                    continue;
+                }
+
+                _resultList.Add(current.val);
+
+                if (current.right != null) stack.Push(current.right);
+                
+            }
+        }
+
+    }
+}
---
+++ Tests/DataStructures/Tree/BinaryTreeInOrderTraversal/Tests.cs
@@ -0,0 +1,77 @@
+using FluentAssertions;
+using NUnit.Framework;
+using Problems.DataStructures.Tree.BinaryTreeInOrderTraversal;
+
+namespace Tests.DataStructures.Tree.BinaryTreeInOrderTraversal
+{
+  public class Tests
+  {
+    [Test]
+    public void TraverseEmptyNode()
+    {
+      var t = new Solution();
+      var result = t.InOrderTraversal(null);
+      result.Count.Should().Be(0);
+    }
+
+    [Test]
+    public void TraverseSingleNode()
+    {
+      var t = new Solution();
+      var root = new TreeNode(1);
+
+      var result = t.InOrderTraversal(root);
+      result.Count.Should().Be(1);
+    }
+
+    [Test]
+    public void TraverseTwoNodesLeft()
+    {
+      var t = new Solution();
+      var root = new TreeNode(1, left: new TreeNode(val: 2));
+
+      var result = t.InOrderTraversal(root);
+      result.Count.Should().Be(2);
+      result[0].Should().Be(2);
+      result[1].Should().Be(1);
+    }
+
+    [Test]
+    public void TraverseTwoNodesRight()
+    {
+      var t = new Solution();
+      var root = new TreeNode(1, right: new TreeNode(val: 2));
+
+      var result = t.InOrderTraversal(root);
+      result.Count.Should().Be(2);
+      result[0].Should().Be(1);
+      result[1].Should().Be(2);
+    }
+
+        [Test]
+    public void TraverseThreeNodes()
+    {
+      var t = new Solution();
+      var root = new TreeNode(1, right: new TreeNode(val: 2, left: new TreeNode(3)));
+
+      var result = t.InOrderTraversal(root);
+      result.Count.Should().Be(3);
+      result[0].Should().Be(1);
+      result[1].Should().Be(3);
+      result[2].Should().Be(2);
+    }
+
+        [Test]
+        public void TraverseThreeNodesB()
+        {
+            var t = new Solution();
+            var root = new TreeNode(3, left: new TreeNode(1), right: new TreeNode(val: 2));
+
+            var result = t.InOrderTraversal(root);
+            result.Count.Should().Be(3);
+            result[0].Should().Be(1);
+            result[1].Should().Be(3);
+            result[2].Should().Be(2);
+        }
+    }
+}
