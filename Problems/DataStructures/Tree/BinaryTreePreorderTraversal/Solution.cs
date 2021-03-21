cfac406fd973af7ae7861d680b3928ca330ee48c
Jeffrey Johnson <jefjohms@gmail.com>
2021-03-21 13:42:38 -0700

    Completed Binary tree pre-order traversal

--- Problems/DataStructures/Tree/BinaryTreePreorderTraversal/Solution.cs
+++ Problems/DataStructures/Tree/BinaryTreePreorderTraversal/Solution.cs
@@ -17,24 +17,62 @@ using System;
 
 namespace Problems.DataStructures.Tree.BinaryTreePreorderTraversal
 {
-  public class Solution
-  {
-    public IList<int> PreorderTraversal(TreeNode root)
+    public class TreeNode
     {
-      return new List<int>(){1};
+        public int Val;
+        public TreeNode Left;
+        public TreeNode Right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.Val = val;
+            this.Left = left;
+            this.Right = right;
+        }
     }
 
-  }
-  public class TreeNode
-  {
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    public class Solution
     {
-      this.val = val;
-      this.left = left;
-      this.right = right;
+        private static List<int> _resultList;
+
+        public IList<int> PreorderTraversal(TreeNode root, bool useRecursive = false)
+        {
+            _resultList = new List<int>();
+            if (useRecursive)
+            {
+                PreOrderRecursive(root);
+            }
+            else
+            {
+              PreOrderIterative(root);
+            }
+            return _resultList;
+
+        }
+
+        private void PreOrderRecursive(TreeNode root)
+        {
+            if (root == null) return;
+
+            _resultList.Add(root.Val);
+            PreOrderRecursive(root.Left);
+            PreOrderRecursive(root.Right);
+        }
+
+        private void PreOrderIterative(TreeNode root)
+        {
+            if (root == null) return;
+
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                _resultList.Add(current.Val);
+                if (current.Right != null) stack.Push(current.Right);
+                if (current.Left != null) stack.Push(current.Left);
+                
+            }
+        }
+
     }
-  }
 }
--- Tests/DataStructures/Tree/BinaryTreePreorderTraversal/Tests.cs
+++ Tests/DataStructures/Tree/BinaryTreePreorderTraversal/Tests.cs
@@ -23,5 +23,55 @@ namespace Tests.DataStructures.Tree.BinaryTreePreorderTraversal
       var result = t.PreorderTraversal(root);
       result.Count.Should().Be(1);
     }
-  }
+
+    [Test]
+    public void TraverseTwoNodesLeft()
+    {
+      var t = new Solution();
+      var root = new TreeNode(1, left: new TreeNode(val: 2));
+
+      var result = t.PreorderTraversal(root);
+      result.Count.Should().Be(2);
+      result[0].Should().Be(1);
+      result[1].Should().Be(2);
+    }
+
+    [Test]
+    public void TraverseTwoNodesRight()
+    {
+      var t = new Solution();
+      var root = new TreeNode(1, right: new TreeNode(val: 2));
+
+      var result = t.PreorderTraversal(root);
+      result.Count.Should().Be(2);
+      result[0].Should().Be(1);
+      result[1].Should().Be(2);
+    }
+
+        [Test]
+    public void TraverseThreeNodes()
+    {
+      var t = new Solution();
+      var root = new TreeNode(1, right: new TreeNode(val: 2, left: new TreeNode(3)));
+
+      var result = t.PreorderTraversal(root);
+      result.Count.Should().Be(3);
+      result[0].Should().Be(1);
+      result[1].Should().Be(2);
+      result[2].Should().Be(3);
+    }
+
+        [Test]
+        public void TraverseThreeNodesB()
+        {
+            var t = new Solution();
+            var root = new TreeNode(3, left: new TreeNode(1), right: new TreeNode(val: 2));
+
+            var result = t.PreorderTraversal(root);
+            result.Count.Should().Be(3);
+            result[0].Should().Be(3);
+            result[1].Should().Be(1);
+            result[2].Should().Be(2);
+        }
+    }
 }
