badfb3b5d3caf5f842380fc54296a72b23ad8b60
Jeffrey Johnson <jefjohms@gmail.com>
2021-03-28 17:09:16 -0700

    Completed Greedy Dice Scoring Kata

---
+++ Problems/GreedyDiceGame/Kata.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.GreedyDiceGame
+{
+    public static class Kata
+    {
+        public static int Score(int[] dice)
+        {
+            var counts = new Dictionary<int, int>();
+            var score = 0;
+            foreach (var die in dice)
+            {
+                if (!counts.ContainsKey(die)) counts[die] = 1;
+                else ++counts[die];
+            }
+
+            foreach (var die in counts.Keys)
+            {
+                var count = counts[die];
+
+                if(count >= 3)
+                {
+                    var tripleScore = die == 1 ? 1000 : die * 100;
+                    score += tripleScore;
+
+                    count -= 3;
+                }
+
+                if(die == 1)
+                {
+                    score += count * 100;
+                }
+
+                if(die == 5)
+                {
+                    score += count * 50;
+                }
+            }
+
+            return score;
+        }
+    }
+}
+    
---
+++ Tests/GreedyDiceGame/Tests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Problems.GreedyDiceGame;
+
+namespace Tests.GreedyDiceGame
+{
+    public class Tests
+    {
+        [Test]
+        public void ScoreRollOne()
+        {
+            var roll = new int[] { 5, 1, 3, 4, 1 };
+            var result = Kata.Score(roll);
+
+            result.Should().Be(250);
+        }
+
+        [Test]
+        public void ScoreRollTwo()
+        {
+            var roll = new int[] { 1, 1, 1, 3, 1 };
+            var result = Kata.Score(roll);
+
+            result.Should().Be(1100);
+        }
+
+        [Test]
+        public void ScoreRollThree()
+        {
+            var roll = new int[] { 2, 4, 4, 5, 4 };
+            var result = Kata.Score(roll);
+
+            result.Should().Be(450);
+        }
+
+    }
+
+
+}
